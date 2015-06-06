31ac5c66cd33b72bdc8657a4bdb661781f9ea09f
Lahaxe <lahaxe@unistra.fr>
2015-06-06 15:23:22 +0200

    ajout de la page records

--- Resultatsathlrecords.php
+++ Resultatsathlrecords.php
@@ -3,6 +3,7 @@
 
     // Ajouter ici les feuilles de style spécifiques
     echo "\n    <link rel=\"stylesheet\" media=\"screen\" type=\"text/css\" title=\"design\" href=\"CSS/style_resultat.css\" />";
+    echo "\n    <link rel=\"stylesheet\" media=\"screen\" type=\"text/css\" title=\"design\" href=\"CSS/style_records.css\" />";
 
     require_once "header_end.php";
 
@@ -16,6 +17,119 @@
    <!-- _______________________________________________________________________________________________________________________________________________________ -->
 			
         <h1>Page en cours de création</h1>
+        
+        <h1>Records de Notre-Dame de Peltre</h1>
+        <h3 class="filles">Benjamines</h3>
+        <div id="records">
+        <table align="center">
+            <tr>
+                <th>Discipline</th>
+                <th>Nom</th>
+                <th class="record">Records</th>
+                <th class="record">Date</th>
+                <th>Lieu</th>
+            </tr>
+            <tr>
+                <td>50 mètres</td>
+                <td class="nomeleve">BALLIGAND Aurélie</td>
+                <td>7''10</td>
+                <td>12-05-2004</td>
+                <td>Champigneulles</td>
+            </tr>
+            <tr>
+                <td>60 mètres</td>
+                <td class="nomeleve">VIBRAC Emilie</td>
+                <td>9''65</td>
+                <td>03-05-1995</td>
+                <td>Sarreguemines</td>
+            </tr>
+            <tr>
+                <td>100 mètres</td>
+                <td class="nomeleve">FAURE Chloé</td>
+                <td>14''00</td>
+                <td>12-05-2004</td>
+                <td>Champigneulles</td>
+            </tr>
+            <tr>
+                <td>120 mètres</td>
+                <td class="nomeleve">COMTE Mélissa</td>
+                <td>19''19</td>
+                <td>07-04-1999</td>
+                <td>Thionville</td>
+            </tr>
+            <tr>
+                <td>1000 mètres</td>
+                <td class="nomeleve">DEPRET Catherine</td>
+                <td>3'46''73</td>
+                <td>15-05-2002</td>
+                <td>Thionville</td>
+            </tr>
+            <tr>
+                <td>2000 mètres</td>
+                <td class="nomeleve">-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>50 mètres haies</td>
+                <td class="nomeleve">FAURE Chloé</td>
+                <td>8''60</td>
+                <td>12-05-2004</td>
+                <td>Champigneulles</td>
+            </tr>
+            <tr>
+                <td>Saut en hauteur</td>
+                <td class="nomeleve">BALLIGAND Aurélie<br/>FILLGRAFF Jennifer<br/>RHIM Stacy</td>
+                <td>1 m 25</td>
+                <td>12-05-2004<br/>13-04-2005<br/>19-04-2006</td>
+                <td>Champigneulles<br/>Thionville<br/>Thionville</td>
+            </tr>
+            <tr>
+                <td>Saut en longueur</td>
+                <td class="nomeleve">LANIOT Adeline</td>
+                <td>4 m 07</td>
+                <td>13-04-2005</td>
+                <td>Thionville</td>
+            </tr>
+            <tr>
+                <td>Triple saut</td>
+                <td class="nomeleve">ZEMLIC Olivia</td>
+                <td>7 m 83</td>
+                <td>18-05-1994</td>
+                <td>Thionville</td>
+            </tr>
+            <tr>
+                <td>Poids</td>
+                <td class="nomeleve">MENU Nina</td>
+                <td>9 m 73</td>
+                <td>01-04-2015</td>
+                <td>Guénange</td>
+            </tr>
+            <tr>
+                <td>Disque</td>
+                <td class="nomeleve">BALLIGAND Aurélie</td>
+                <td>16 m 83</td>
+                <td>12-05-2004</td>
+                <td>Champigneulles</td>
+            </tr>
+            <tr>
+                <td>Javelot</td>
+                <td class="nomeleve">-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Relais 4x60</td>
+                <td class="nomeleve">RHINGAND Elisabeth<br/>COTTY Marine<br/>DEPRET Catherine<br/>HOCINE Ouacila</td>
+                <td>34''45</td>
+                <td>15-05-2002</td>
+                <td>Thionville</td>
+            </tr>
+        </table>
+        </div>
+        <br/>
             
    <!-- _______________________________________________________________________________________________________________________________________________________ -->
    
